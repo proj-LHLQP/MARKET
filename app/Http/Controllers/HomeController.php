0bde49743075df73f027756f6b6031a5e60b8ca0
Nguyễn Minh Quang <nmquang21@gmail.com>
2019-08-17 14:49:58 +0700

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
+
 
 class HomeController extends Controller
 {
